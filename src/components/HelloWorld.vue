950c58f74e970e10bd92c5c6a42a7dadab3d1f01
axelf <axelf@theodo.fr>
2021-02-19 21:51:46 +0100

    first commit

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,32 @@
+<template>
+  <h1>{{ msg }}</h1>
+  <button @click="count++">count is: {{ count }}</button>
+</template>
+
+<script setup lang="ts">
+import { defineProps } from "vue";
+
+defineProps({
+  msg: String,
+});
+
+ref: count = 0;
+</script>
+
+<style scoped>
+a {
+  color: #42b983;
+}
+
+label {
+  margin: 0 0.5em;
+  font-weight: bold;
+}
+
+code {
+  background-color: #eee;
+  padding: 2px 4px;
+  border-radius: 4px;
+  color: #304455;
+}
+</style>
