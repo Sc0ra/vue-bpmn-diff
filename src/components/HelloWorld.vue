d0575405ca97d996640e063994c1dae1c2755a93
axelf <axelf@theodo.fr>
2021-02-19 22:02:58 +0100

    chore: rename package

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -4,7 +4,7 @@
 </template>
 
 <script setup lang="ts">
-import { defineProps } from "vue";
+import { defineProps, ref } from "vue";
 
 defineProps({
   msg: String,
@@ -13,20 +13,4 @@ defineProps({
 ref: count = 0;
 </script>
 
-<style scoped>
-a {
-  color: #42b983;
-}
-
-label {
-  margin: 0 0.5em;
-  font-weight: bold;
-}
-
-code {
-  background-color: #eee;
-  padding: 2px 4px;
-  border-radius: 4px;
-  color: #304455;
-}
-</style>
+<style scoped></style>
