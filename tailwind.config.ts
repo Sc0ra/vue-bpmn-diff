e9e437505866b69f06fa417f2391e7694f3575bb
axelf <axelf@theodo.fr>
2021-02-22 19:00:44 +0100

    feat: replace tailwind by windicss

---
+++ tailwind.config.ts
@@ -0,0 +1,14 @@
+import { defineConfig } from "vite-plugin-windicss";
+
+export default defineConfig({
+  darkMode: "class",
+  theme: {
+    extend: {
+      colors: {
+        teal: {
+          100: "#096",
+        },
+      },
+    },
+  },
+});
